81d569808934ce00f3dc78e2b4b2c3190636768d
Denysson Max <maxdenyssonn@gmail.com>
2022-11-22 00:36:04 -0300

    atualizacao pg concorrente

--- programacaoConcorrente/src/akka/start.java
+++ programacaoConcorrente/src/akka/start.java
@@ -1,5 +1,24 @@
 package programacaoConcorrente.src.akka;
 
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+
 public class start {
-    
+
+	public static void main(String[] args) {
+
+
+	
+		ActorSystem system = ActorSystem.create("HelloSystem");
+
+	
+		ActorRef ecoActor = system.actorOf(Props.create(ecoActor.class), "eco");
+
+		
+		ecoActor.tell(new helloMessage(), ActorRef.noSender());
+
+
+	}
+
 }
