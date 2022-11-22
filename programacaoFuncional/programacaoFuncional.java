60ce52560ebad32b0122d9c24eb459925037ebbb
Denysson Max <maxdenyssonn@gmail.com>
2022-11-22 00:08:56 -0300

    acrescentando programação funcional

--- programacaoFuncional/programacaoFuncional.java
+++ programacaoFuncional/programacaoFuncional.java
@@ -1,2 +1,34 @@
 package programacaoFuncional;
 
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class programacaoFuncional {
+    public static void main(String[] args) {
+        String[] animals = {"Elefante", "Gato", "Cachorro", "Agua-viva"};
+
+        List<String> animalsList = programacaoFuncional.getList(animals);
+        Set<String> animalsSet = programacaoFuncional.getSet(animals);
+        Map<String, Integer> animalsMap = programacaoFuncional.getMap(animalsList);
+
+        System.out.println("\nList");
+        animalsList.forEach(System.out::println);
+
+        System.out.println("\nSet");
+        animalsSet.forEach(System.out::println);
+
+        System.out.println("\nMap");
+        animalsMap.forEach((key, value) -> {
+            System.out.println(key + ": " + value);
+        });
+
+        System.out.println("\nCounting\n" + programacaoFuncional.getLength(animals));
+
+        System.out.println("\nJoining\n" + programacaoFuncional.getJoinedString(animals));
+    }
+
+   
+}
+
+
