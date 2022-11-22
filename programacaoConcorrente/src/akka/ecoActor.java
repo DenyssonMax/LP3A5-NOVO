e41cd4212829dcafb0998336853eebc44cc1b8db
Denysson Max <maxdenyssonn@gmail.com>
2022-11-22 00:34:16 -0300

    update pgconcorrente

--- programacaoConcorrente/src/akka/ecoActor.java
+++ programacaoConcorrente/src/akka/ecoActor.java
@@ -1,5 +1,37 @@
 package programacaoConcorrente.src.akka;
 
-public class ecoActor {
-    
+import akka.actor.ActorRef;
+import akka.actor.Props;
+import akka.actor.UntypedActor;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+
+public class ecoActor extends UntypedActor {
+
+    LoggingAdapter log = Logging.getLogger(getContext().system(), this);
+
+   
+    private actorRef childActor;
+
+    @Override
+    public void preStart() throws Exception {
+        super.preStart();
+
+       
+        childActor = getContext().actorOf(Props.create(childActor.class), "childOfEco");
+    }
+
+    @Override
+    public void onReceive(Object msg) throws Exception {
+
+        if (msg instanceof helloMessage) {
+            log.info("Mensagem recebida: " + msg);
+            
+            childActor.tell(msg, getSelf());
+        }
+        else {
+            
+            unhandled(msg);
+        }
+    }
 }
